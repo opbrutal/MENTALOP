927d60e2160303e40af353e47ccdd91a4a83f435
ophexor <89744095+ophexor@users.noreply.github.com>
2021-11-20 09:38:56 +0530

    Add files via upload

--- handlers/play.py
+++ handlers/play.py
@@ -53,8 +53,8 @@ def changeImageSize(maxWidth, maxHeight, image):
     return newImage
 
 async def generate_cover(requested_by, title, views, duration, thumbnail):
-async with aiohttp.ClientSession() as session:
-async with session.get(thumbnail) as resp:
+ async with aiohttp.ClientSession() as session:
+ async with session.get(thumbnail) as resp:
     if resp.status == 200:
     f = await aiofiles.open("background.png", mode="wb")
     await f.write(await resp.read())
