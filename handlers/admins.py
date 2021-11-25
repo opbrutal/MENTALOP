826767d60c0727c263687103f268a32908d2d906
ophexor <89744095+ophexor@users.noreply.github.com>
2021-11-25 14:25:55 +0530

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -71,12 +71,12 @@ async def skip(_, message: Message):
         if callsmusic.queues.is_empty(message.chat.id):
             callsmusic.pytgcalls.leave_group_call(message.chat.id)
     else:
-         await callsmusic.pytgcalls.change_stream(
-           chat_id, 
-             InputStream(
-               InputAudioStream(
-                 callsmusic.queues.get(chat_id)["file"],
-            ),
-          ),
-        )
+     await callsmusic.pytgcalls.change_stream(
+     chat_id, 
+     InputStream(
+     InputAudioStream(
+     callsmusic.queues.get(chat_id)["file"],
+     ),
+     ),
+     )
         await message.reply_text("➡️ 𝐒𝐤𝐢𝐩 💫 𝐓𝐡𝐞 𝐂𝐮𝐫𝐫𝐞𝐧𝐭 ✨ 𝐒𝐨𝐧𝐠 🥀")
