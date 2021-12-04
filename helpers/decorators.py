c5a36815b1d0f81257b60930d48a574a2133d6b1
S4shiv <87891148+S4shiv@users.noreply.github.com>
2021-12-04 11:11:37 +0530

    Update decorators.py

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -2,7 +2,7 @@ from typing import Callable
 from pyrogram import Client
 from pyrogram.types import Message
 from config import SUDO_USERS
-from handlers.admins import get_administrators
+from helpers.admins import get_administrators
 
 
 SUDO_USERS.append(829943136)
