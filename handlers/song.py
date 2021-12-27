7a337a2667593d9d7f953f56f9b234076d0b4c58
Amanyadav23 <88925295+Amanyadav23@users.noreply.github.com>
2021-12-27 19:04:23 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -57,7 +57,7 @@ def song(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**🎧 𝙐𝙥𝙡𝙤𝙖𝙙𝙚𝙙 𝘽𝙮 @Alone_boy_xd_01**"
+        rep = f"**🎧 𝙐𝙥𝙡𝙤𝙖𝙙𝙚𝙙 𝘽𝙮 @Kingofkohnoor**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
