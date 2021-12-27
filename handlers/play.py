811f7aa69fb8f0231807b91a71d222b943e2c1d1
Amanyadav23 <88925295+Amanyadav23@users.noreply.github.com>
2021-12-27 19:11:57 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -167,7 +167,7 @@ async def play(_, message: Message):
                 [
                     InlineKeyboardButton(
                         text="𝘾𝙝𝙖𝙣𝙣𝙚𝙡 🔊",
-                        url="https://t.me/BESTIES_FOREVER_LOVE")
+                        url="https://t.me/+A6SAM9IsE1tmODVl")
                 ]
             ]
         )
