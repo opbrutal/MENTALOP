7b8b83824a1f194e9f2483b249a6061f77017d2e
Amanyadav23 <88925295+Amanyadav23@users.noreply.github.com>
2022-06-08 22:49:52 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -9,9 +9,8 @@ from helpers.filters import other_filters2
 async def start(_, message: Message):
     await message.reply_sticker("CAADBQADRQIAAhHHQFSfHJ-IR0eN6gI")
     await message.reply_text(
-        f"""**- 𝙃𝙚𝙮 𝘼𝙢 {bn} 💛🐬,
-
-- 𝙄 𝙘𝙖𝙣 𝙥𝙡𝙖𝙮 𝙢𝙪𝙨𝙞𝙘 𝙞𝙣 𝙮𝙤𝙪𝙧 𝙜𝙧𝙤𝙪𝙥'𝙨 𝙫𝙤𝙞𝙘𝙚 𝙘𝙖𝙡𝙡. 𝘿𝙚𝙫𝙚𝙡𝙤𝙥𝙚𝙙 𝙗𝙮 [𝘼𝙠](https://t.me/ALONE_BOY_XD_01) 💛🤞.
+        f"""**- 𝙃𝙚𝙮 𝘼𝙢 {𝙈𝙀𝙉𝙏𝘼𝙇 𝙭 𝙌𝙐𝙀𝙀𝙉} 💛🐬,
+- 𝙄 𝙘𝙖𝙣 𝙥𝙡𝙖𝙮 𝙢𝙪𝙨𝙞𝙘 𝙞𝙣 𝙮𝙤𝙪𝙧 𝙜𝙧𝙤𝙪𝙥'𝙨 𝙫𝙤𝙞𝙘𝙚 𝙘𝙖𝙡𝙡. 𝘿𝙚𝙫𝙚𝙡𝙤𝙥𝙚𝙙 𝙗𝙮 [𝙈𝙀𝙉𝙏𝘼𝙇](https://t.me/OFFICIAL_MENTALMOD) 💛🤞.
 
 𝘼𝙙𝙙 𝙢𝙚 𝙩𝙤 𝙮𝙤𝙪𝙧 𝙜𝙧𝙤𝙪𝙥 𝙖𝙣𝙙 𝙥𝙡𝙖𝙮 𝙢𝙪𝙨𝙞𝙘 𝙛𝙧𝙚𝙚𝙡𝙮 🐬💕**
         """,
@@ -19,17 +18,17 @@ async def start(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                         " 𝙊𝙬𝙣𝙚𝙧 ", url="https://t.me/Kingofkohnoor")
+                         " 𝙊𝙬𝙣𝙚𝙧 ", url="https://t.me/OFFICIAL_MENTALMOD")
                   ],[
                     InlineKeyboardButton(
-                        "😈  𝙎𝙪𝙥𝙥𝙤𝙧𝙩", url="https://t.me/A_4_AMAN_0fficial"
+                        "😈  𝙎𝙪𝙥𝙥𝙤𝙧𝙩", url="https://t.me/A_4_AMAN_0fficcial"
                     ),
                     InlineKeyboardButton(
-                        "✌️ 𝙂𝙍𝙊𝙐𝙋", url="https://t.me/+A6SAM9IsE1tmODVl"
+                        "✌️ 𝙂𝙍𝙊𝙐𝙋", url="https://t.me/MENTAL_MOD"
                     )
                 ],[ 
                     InlineKeyboardButton(
-                        "➕ 𝘼𝙙𝙙 𝙏𝙤 𝙔𝙤𝙪𝙧 𝙂𝙧𝙤𝙪𝙥 ➕", url="https://t.me/AK_XD_MUSIC_BOT?startgroup=true"
+                        "➕ 𝘼𝙙𝙙 𝙏𝙤 𝙔𝙤𝙪𝙧 𝙂𝙧𝙤𝙪𝙥 ➕", url="https://t.me/MENTALxQUEEN_MUSIC_BOT?startgroup=true"
                     )]
             ]
         ),
@@ -38,12 +37,12 @@ async def start(_, message: Message):
 
 @Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
-      await message.reply_text("""** 𝘼𝙡𝙤𝙣𝙚 𝙈𝙪𝙨𝙞𝙘 𝙋𝙡𝙖𝙮𝙚𝙧 𝙄𝙨 𝙊𝙣𝙡𝙞𝙣𝙚 ✅**""",
+      await message.reply_text("""** 𝙈𝙀𝙉𝙏𝘼𝙇 𝙭 𝙌𝙐𝙀𝙀𝙉 𝙈𝙪𝙨𝙞𝙘 𝙋𝙡𝙖𝙮𝙚𝙧 𝙄𝙨 𝙊𝙣𝙡𝙞𝙣𝙚 ✅**""",
       reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🔊 𝙈𝙖𝙣𝙖𝙜𝙚𝙧", url="https://t.me/A_4_AMAN_0fficial")
+                        "🔊 𝙈𝙖𝙣𝙖𝙜𝙚𝙧", url="https://t.me/A_4_AMAN_Officcial")
                 ]
             ]
         )
